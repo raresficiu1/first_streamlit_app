f63cbd7e249415479a9674b2e4651fed9dd46719
Rares <36771504+raresficiu1@users.noreply.github.com>
2022-09-05 23:19:44 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,11 @@ variabile='kiwi'
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{variabile}")
+
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_choice}")
 streamlit.text(fruityvice_response.json())
 
 
