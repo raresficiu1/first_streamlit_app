7b153fce721714677ea5f5facbe7631d6cdb5c8c
Rares <36771504+raresficiu1@users.noreply.github.com>
2022-09-05 23:16:44 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
+import streamlit
+import requests
+
 streamlit.header("Fruityvice Fruit Advice!")
 
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
